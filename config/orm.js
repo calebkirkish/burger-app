6dee0a4d5f3d161a901cceda2c178d3b5c33300d
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-09-14 21:00:44 -0700

    added helper code to the orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,59 @@
 var connection = require('./connection.js');
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++){
+        arr.push('?');
+    }
+    return arr.toString();
+}
+
+function objToSql(ob){
+    var arr = [];
+
+    for (var key in ob){
+        var value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if(typeof value === "string" && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + '=' + value);
+        }
+    }
+    return arr.toString();
+}
+
 var orm = {
     selectAll: function(tableInput, cb){
         console.log(tableInput);
         var queryString = "Select * From ??"
-        connection.query(queryString, [tableInput], function(err, result) {
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             console.log(result);
             cb(result);
         });
     },
-//     insertOne: function(whatToSelect, )
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
     
     
 }
