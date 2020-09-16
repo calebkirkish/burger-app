f5699785c8bc760a58c4cb41297b555f1ed224df
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-09-16 11:10:24 -0700

    added the same functionalities of update & delete to the models from orm

--- models/burger.js
+++ models/burger.js
@@ -6,5 +6,23 @@ const burgers = {
       cb(res);
     });
   },
-}
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
 module.exports = burgers
